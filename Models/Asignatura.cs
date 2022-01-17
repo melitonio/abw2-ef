009617fa9b7f1263621636e27fa9585d3d6c1057
Meliton Pablo <melitonio8@hotmail.com>
2022-01-17 06:58:39 +0100

    Mejoras lijeras

--- Models/Asignatura.cs
+++ Models/Asignatura.cs
@@ -1,39 +1,39 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WebAppNetCore.Models
+namespace WebAppNetCore.Models;
+
+[Table("Asignaturas")]
+public class Asignatura
 {
-    [Table("Asignaturas")]
-    public class Asignatura
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public int AsignaturaID { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public int AsignaturaID { get; set; }
 
-        [MaxLength(6)]
-        public string? Codigo { get; set; }
+    [MaxLength(6)]
+    public string? Codigo { get; set; }
 
-        [Required]
-        [MaxLength(70)]
-        public string? Nombre { get; set; }
+    [Required]
+    [MaxLength(70)]
+    public string? Nombre { get; set; }
 
-        [Required]
-        public int? Creditos { get; set; }
+    [Required]
+    public int? Creditos { get; set; }
 
-        [ForeignKey("Carrera")]
-        public int CarreraID { get; set; }
-        public Carrera? Carrera { get; set; }
+    [ForeignKey("Carrera")]
+    public int CarreraID { get; set; }
+    public Carrera? Carrera { get; set; }
 
-        [Range(1, 4)]
-        [Required]
-        public int? Curso { get; set; }
+    [Range(1, 4)]
+    [Required]
+    public int? Curso { get; set; }
 
-        [Range(1, 2)]
-        public int? Semestre { get; set; }
+    [Range(1, 2)]
+    public int? Semestre { get; set; }
 
-        [Required]
-        public Carracter? Carracter { get; set; }
-    }
-    
-    public enum Carracter { Basica, Obligatoria, Optativa }
+    [Required]
+    public Carracter? Carracter { get; set; }
 }
+
+public enum Carracter { Basica, Obligatoria, Optativa }
